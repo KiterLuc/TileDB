493c11e29fdc04faf9c331a5512b383b3030db2f
Theodore Tsirpanis <theodore.tsirpanis@tiledb.com>
2023-12-11 23:24:17 +0200

    Remove the superbuild and the external projects.

--- cmake/Options/BuildOptions.cmake
+++ cmake/Options/BuildOptions.cmake
@@ -7,9 +7,6 @@
 
 include(CMakeDependentOption)
 
-option(TILEDB_SUPERBUILD "If true, perform a superbuild (builds all missing dependencies)." ON)
-option(TILEDB_VCPKG "If true, use vcpkg to download and build dependencies." ON)
-cmake_dependent_option(TILEDB_FORCE_ALL_DEPS "If true, force superbuild to download and build all dependencies, even those installed on the system." OFF "NOT TILEDB_VCPKG" OFF)
 option(TILEDB_REMOVE_DEPRECATIONS "If true, do not build deprecated APIs." OFF)
 option(TILEDB_VERBOSE "Prints TileDB errors with verbosity" OFF)
 option(TILEDB_S3 "Enables S3/minio support using aws-cpp-sdk" OFF)
@@ -37,10 +34,6 @@ option(TILEDB_TESTS_ENABLE_REST "Enables REST tests (requires running REST serve
 option(CMAKE_EXPORT_COMPILE_COMMANDS "cmake compile commands" ON)
 
 set(TILEDB_INSTALL_LIBDIR "" CACHE STRING "If non-empty, install TileDB library to this directory instead of CMAKE_INSTALL_LIBDIR.")
-
-if (NOT TILEDB_VCPKG)
-  message(FATAL_ERROR "Disabling TILEDB_VCPKG is not supported.")
-endif()
 
 # enable assertions by default for debug builds
 if (CMAKE_BUILD_TYPE STREQUAL "Debug")
